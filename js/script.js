c720f2a0cf0e49e5d0a0ab160b0a995f5e783e55
pakdaniil <pakdaniil007@gmail.com>
2024-05-02 20:37:09 +0600

    init

---
+++ js/script.js
@@ -0,0 +1,27 @@
+// $(document).ready(function(){
+//     $(".owl-carousel").owlCarousel({
+//         items:2,
+      
+//     });
+//   });
+
+  $(document).ready(function(){
+    $(".slide-one").owlCarousel({
+      items:2,
+      
+    });
+  });
+
+  $(document).ready(function(){
+    $(".slide-two").owlCarousel({
+      items:3
+      ,
+      margin:20,
+  
+    });
+  });
+
+  Fancybox.bind('[data-fancybox="gallery"]', {
+    // Your custom options for a specific gallery
+  });
+  
